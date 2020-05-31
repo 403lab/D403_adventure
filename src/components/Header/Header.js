1133c4e5a1bf3e894e5be6bce4d1911f2459b874
KuuWangE <shellcodesniper@icloud.com>
2020-05-31 21:12:10 +0900

    Add Header template

---
+++ src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+// import '../../stylesheets/Main.css'
+import {
+  Button
+} from 'reactstrap';
+
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+
+  }
+
+  componentDidMount() {
+    console.log("Mounted.")
+    // this.props.history.push("/");
+  }
+
+  render() {
+    return ( <
+      div className = "Main" >
+      <
+      Button / >
+      <
+      /div>
+    );
+  }
+}
+
+export default Main;
